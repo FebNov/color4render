1904e3ba4660e83f1bbc3efe6142c8c9822c0a5e
Hunterprey <595129026@qq.com>
2021-02-27 15:33:24 +1000

    add comment

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -15,22 +15,35 @@ const Container = styled.div`
 `
 function Layout(){
     let eightSteps = []
-    let unOrderEightSteps = []
-    const  maxLength = 33;
     let totalItems = [];
     for(let i=0;i<=256 ;i+=8){
         eightSteps.push(i)
     }
-    for (let i= 0 ; i < maxLength ;i++ ){
-        const randomIndex = Math.floor(Math.random() * Math.floor(eightSteps.length));
-        let randomValue = eightSteps[randomIndex]
-        unOrderEightSteps.push(randomValue)
-        eightSteps.splice(randomIndex,1)
-    }
+    // below is random order color 
+    //================================
+    // let unOrderEightSteps = []
+    // const  maxLength = 33;
+    // for (let i= 0 ; i < maxLength ;i++ ){
+    //     const randomIndex = Math.floor(Math.random() * Math.floor(eightSteps.length));
+    //     let randomValue = eightSteps[randomIndex]
+    //     unOrderEightSteps.push(randomValue)
+    //     eightSteps.splice(randomIndex,1)
+    // }
+
+    // this is for random color 
+    // for(let i=0;i < 32 ;i++){
+    //     for(let j=0;j < 32 ;j++){
+    //         for(let k=0;k < 32 ;k++){
+    //             totalItems.push(<Item redEl={unOrderEightSteps[i]} greenEl={unOrderEightSteps[j]} blueEl={unOrderEightSteps[k]}/>);
+    //         }
+    //     }
+    // }
+
+    // this is for order color 0-8-16-24-32-...256
     for(let i=0;i < 32 ;i++){
         for(let j=0;j < 32 ;j++){
             for(let k=0;k < 32 ;k++){
-                totalItems.push(<Item redEl={unOrderEightSteps[i]} greenEl={unOrderEightSteps[j]} blueEl={unOrderEightSteps[k]}/>);
+                totalItems.push(<Item redEl={eightSteps[i]} greenEl={eightSteps[j]} blueEl={eightSteps[k]}/>);
             }
         }
     }
