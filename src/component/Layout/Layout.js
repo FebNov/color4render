db378e17d5c9ebbb10f2b8c9337230c853f12c91
Hunterprey <595129026@qq.com>
2021-02-28 23:47:00 +1000

    1

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -16,37 +16,37 @@ const Container = styled.div`
 function Layout(){
     let eightSteps = []
     let totalItems = [];
-    for(let i=0;i<=256 ;i+=8){
+    for(let i=8;i<=256 ;i+=8){
         eightSteps.push(i)
     }
     // below is random order color 
     //================================
-    // let unOrderEightSteps = []
-    // const  maxLength = 33;
-    // for (let i= 0 ; i < maxLength ;i++ ){
-    //     const randomIndex = Math.floor(Math.random() * Math.floor(eightSteps.length));
-    //     let randomValue = eightSteps[randomIndex]
-    //     unOrderEightSteps.push(randomValue)
-    //     eightSteps.splice(randomIndex,1)
-    // }
+    let unOrderEightSteps = []
+    const  maxLength = 33;
+    for (let i= 0 ; i < maxLength ;i++ ){
+        const randomIndex = Math.floor(Math.random() * Math.floor(eightSteps.length));
+        let randomValue = eightSteps[randomIndex]
+        unOrderEightSteps.push(randomValue)
+        eightSteps.splice(randomIndex,1)
+    }
 
     // this is for random color 
-    // for(let i=0;i < 32 ;i++){
-    //     for(let j=0;j < 32 ;j++){
-    //         for(let k=0;k < 32 ;k++){
-    //             totalItems.push(<Item redEl={unOrderEightSteps[i]} greenEl={unOrderEightSteps[j]} blueEl={unOrderEightSteps[k]}/>);
-    //         }
-    //     }
-    // }
-
-    // this is for order color 0-8-16-24-32-...256
     for(let i=0;i < 32 ;i++){
         for(let j=0;j < 32 ;j++){
             for(let k=0;k < 32 ;k++){
-                totalItems.push(<Item redEl={eightSteps[i]} greenEl={eightSteps[j]} blueEl={eightSteps[k]}/>);
+                totalItems.push(<Item redEl={unOrderEightSteps[i]} greenEl={unOrderEightSteps[j]} blueEl={unOrderEightSteps[k]}/>);
             }
         }
     }
+
+    // this is for order color 8-16-24-32-...255
+    // for(let i=0;i <32 ;i++){
+    //     for(let j=0;j < 32 ;j++){
+    //         for(let k=0;k < 32 ;k++){
+    //             totalItems.push(<Item redEl={eightSteps[i]} greenEl={eightSteps[j]} blueEl={eightSteps[k]}/>);
+    //         }
+    //     }
+    // }
     return (
 		<Wrapper>
 			<Container>{ totalItems }</Container>
